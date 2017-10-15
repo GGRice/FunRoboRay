8ab40da42a3a3521e664e3ba21c2efcedd565421
aselker <aselker319@gmail.com>
2017-10-15 12:32:54 -0400

    Cleaned up, fixed merge issues
    Changed a few logic bits (e.g. if -> ternary) but mostly clarified comments.  Should be shorter, more consistent.

--- FunRobo_P1_V3.ino
+++ FunRobo_P1_V3.ino
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
 /*
- * Main code, rough prototype
- * Not tested, do not use!
+ * Main code, final version
  * Reads from two IR proximity sensors, moves three servos to move the craft
- * Steers away from walls
+ * Steers away from walls, skulls backwards if necessary
  * Team Nemo 9/2017
  */
 
@@ -11,36 +9,34 @@
 #include <Servo.h>
 #include <SharpIR.h>
 
-const int wingPeriodStraight = 8000; //Period of wing straight movement, in milliseconds
-const int wingPeriodTurn = 2500; //Period of wing turning movement, in milliseconds
-const int wingCenter = 90; //Center of the servos' motion
-const int wingRange = 75; //Range of motion, degrees, at full power
+const int wingPeriodStraight = 8000; //Wings' period during straight motion, in milliseconds
+const int wingPeriodTurn = 2500; ///Wings' period during turn, in milliseconds
+const int wingCenter = 90; //Center of the wing servos' motion
+const int wingRange = 75; //Range of servos' motion, in degrees
 
-const int rudderCenter = 87; //Center of the servos' motion
-const int rudderRange = 100; //Rudder range of motion, degrees
+const int rudderCenter = 87; //Center of the rudder servo's motion
+const int rudderRange = 100; //Rudder range of motion, in degrees
 
 const int turnTime = 3000; //How long a turn takes, in milliseconds
-const int skullTime = 6000; //Same but for skulling backwards
+const int skullTime = 6000; //Same but for skulling backwards -- longer because slower
 
 const int sensorAvgWindow = 10; //number of sensor readings to average over to determine obstacle distance
-int leftReadings[sensorAvgWindow], rightReadings[sensorAvgWindow]; //prep to take number of readings for each sensor determined by sensorAvgWindow
-int leftAvg = 0, rightAvg = 0; //set averaged sensor readings for each sensor
+int leftReadings[sensorAvgWindow], rightReadings[sensorAvgWindow]; //The past few readings
+int leftAvg = 0, rightAvg = 0; //Used to average the readings
 
-const int turnThreshhold = 225; //Turning begins when voltage is greater than this
+const int turnThreshhold = 225; //Begin turn when voltage > this
 const int reverseThreshhold = 300; //Skull backwards when voltage > this
-
 
 const int leftSensorPin = A4, rightSensorPin = A5; //Analog pins to read sensors
 const int leftWingPin = 9, rightWingPin = 10, rudderPin = 11; //PWM-capable pins for servos
-const int led = A3; //Output pin to blinking LED
+const int led = A3; //Output pin to blinking LED -- analog pin because of convenient wiring
 
-int left_volt, right_volt; //Analog signals coming from the IR sensors
+int leftVolt, rightVolt; //Analog signals coming from the IR sensors
 
 Servo leftWing, rightWing, rudder; //Servo objects for wings and rudder
 
 int turnDir = 0; //0 for straight, -1 for left, 1 for right, 2 for backwards
 int turnTimer = 0; //Milliseconds left in the turn
-int phase; //Keeps track of the current position in the sine wave for the wings
 long unsigned int timer = 0; //Current position in the wing-motion cycle
 long unsigned int lastTime = 0; //Time the last iteration ended
 long int loopTime = 0; //How long the last loop took -- signed to prevent underflow
@@ -62,35 +58,34 @@ void setup() {
 
 void loop() {
   /*________sense________*/
-  // If turn from last loop isn't finish, this will ensure that it finishes.
-  if (turnTimer>0){
+
+  // Keep track of how long we've been turning, stop if necessary
+  if (turnTimer>0) {
     turnTimer -= loopTime; //Decrement the turn timer, in milliseconds
-    if (turnTimer < 0) turnTimer = 0;
+    if (turnTimer < 0) turnTimer = 0; //Because it might jump 0
   }
-  else {
-    turnDir = 0; //when turn is finished, direction is set to forward
-  }
+  else turnDir = 0; //when turn is finished, direction is set to forward
 
-//directly read sensor voltages
-  left_volt = analogRead(leftSensorPin);
-  right_volt = analogRead(rightSensorPin);
+  leftVolt = analogRead(leftSensorPin); //Read sensor voltages to detect proximity
+  rightVolt = analogRead(rightSensorPin); //This turns out to be more reliable than a library
 
-  Serial.print("Left reading: " + String(left_volt) +"  Right reading: " + String(right_volt));
+  Serial.print("Left reading: " + String(leftVolt) +"  Right reading: " + String(rightVolt));
   
+
   /*________think________*/
 
   for (int i = 0; i < sensorAvgWindow - 1; i++) {
-    leftReadings[i] = leftReadings[i+1]; //Shift all readings back one space
-    rightReadings[i] = rightReadings[i+1];
+    leftReadings[i] = leftReadings[i+1]; //Shift all readings back one space, drop the oldest reading
+    rightReadings[i] = rightReadings[i+1]; //A circular buffer might be faster, but would be more complex
   }
 
-  leftReadings[sensorAvgWindow-1] = left_volt; //store sensor readings
-  rightReadings[sensorAvgWindow-1] = right_volt;
+  leftReadings[sensorAvgWindow-1] = leftVolt; //Add the newest readings
+  rightReadings[sensorAvgWindow-1] = rightVolt;
 
   leftAvg = 0; //Reset the averages, so we can re-sum them
   rightAvg = 0;
 
-  for (int i = 0; i < sensorAvgWindow; i++) { //Sum to average
+  for (int i = 0; i < sensorAvgWindow; i++) { //Sum window
     leftAvg += leftReadings[i];
     rightAvg += rightReadings[i];
   }
@@ -100,54 +95,51 @@ void loop() {
 
   Serial.print("Left avg: " + String(leftAvg) +"  Right avg: " + String(rightAvg));
 
-
-  // This determines our boat's direction by assigning it a switch case based on sensor output
-  if (leftAvg > turnThreshhold){ //Are we too close to a wall on the left side?
+  if (leftAvg > turnThreshhold) { //Are we too close to a wall on the left side?
     turnDir = 1;  //if so, turn to the right
     turnTimer = turnTime;  //Sets us to have a turn of duration determined by turnTime
   }
-  if (rightAvg > turnThreshhold){ //Checks for wall on right side
+  if (rightAvg > turnThreshhold) { //Checks for wall on right side
     turnDir = -1; //if wall is too close, turn left (if both sensors detect a wall, the boat will turn left)
     turnTimer = turnTime;
   }
-  if ((rightAvg > reverseThreshhold) && (leftAvg > reverseThreshhold)){ //If both sensors are too close to a wall, skull backwards
+  if ((rightAvg > reverseThreshhold) && (leftAvg > reverseThreshhold)) { //If both sensors are really close, skull backwards
     turnDir = 2; 
-    turnTimer = skullTime; //skulls for preset time
+    turnTimer = skullTime; //Skulls for preset time
   }
  
-  int rudder_phase = rudderCenter + triangleWaveFunction(timer) * rudderRange / wingRange;
+  int rudderPhase = rudderCenter + triangleWaveFunction(timer) * rudderRange / wingRange; //This only applies while skulling
+
 
   /*_________act_________*/
+
   switch (turnDir) {
-    //In case 0 the boat will go straight because the sensor did not sense a wall within range.
-    case 0:
-      Serial.println("case 0");
+
+    case 0: //Neither sensor triggered
+      Serial.println("Going straight");
       leftWing.write(wingCenter - triangleWaveFunction(timer));
       rightWing.write(wingCenter + triangleWaveFunction(timer));
       rudder.write(rudderCenter);
       Serial.println(rudder.read());
       break;
     
-    //In case -1 the boat will turn to the because the left IR sensed a wall within range. 
-    case -1:
-      Serial.println("case -1");
+    case -1: //Right sensor triggered, but not left
+      Serial.println("Turning left");
       leftWing.write(wingCenter - triangleWaveFunction(timer));
       rudder.write(rudderCenter + rudderRange/2);
       Serial.println(rudder.read());
       Serial.println(rudderCenter - rudderRange);
       break;
       
-    //In case 1 the boat will turn to the because the right IR sensed a wall within range.
-    case 1:
-      Serial.println("case 1");
+    case 1: //Left (and maybe right) triggered
+      Serial.println("Turning right");
       rightWing.write(wingCenter + triangleWaveFunction(timer));
       rudder.write(rudderCenter - rudderRange/2);
       Serial.println(rudder.read());
       break;
     
-    //In case 2 the boat has sensed both walls as being dangerously close and is now using the rudder to reverse.
-    case 2:
-      Serial.println("case 2");
+    case 2: //Both *really* triggered
+      Serial.println("Skulling");
       rudder.write(rudder_phase);
       Serial.println(rudder.read());
       break;
@@ -155,221 +147,16 @@ void loop() {
  
   timer += (millis() - lastTime) * (turnDir ? (wingPeriodStraight / wingPeriodTurn) : 1); //Count faster if we're turning
   timer = timer % wingPeriodStraight; //Prevent overflows
-  loopTime = millis() - lastTime;
+  loopTime = millis() - lastTime; //For decrementing turn timer
   lastTime = millis(); //So we'll know how long the next loop takes
  
- //Blink LED while main loop is running
-  if (timer < wingPeriodStraight/2){
-    digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  }
-  else {
-    digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  }
+  //Blink activity LED by turning it on iff we're in the first half of the period
+  digitalWrite(led, (timer < wingPeriodStraight/2) ? HIGH : LOW);
 }
 
-// This is a function to be used for the wing servos. It is a trunkated triangle wave function. 
-// This will make the servos move at a constant rate and uses a fourier transform to imitate a trunkated triangle wave.
+//This creates a rounded triangle-wave function with period `wingPeriodStraight` and range `wingRange`
+//The rounding is achieved via Fourier transform (done outside this program)
 int triangleWaveFunction(int phase) {
   float x = phase * 2*PI / wingPeriodStraight;
   return wingRange/2 * 8*(PI*PI)*(sin(x)-(1/9*sin(3*x))+(1/25*sin(5*x))-(1/49*sin(7*x))) / 100;
 }
-=======
-/*
- * Main code, rough prototype
- * Not tested, do not use!
- * Reads from two IR proximity sensors, moves three servos to move the craft
- * Steers away from walls
- * Team Nemo 9/2017
- */
- 
-#include <Servo.h>
-#include <SharpIR.h>
-
-const int wingPeriodStraight = 8000; //Period of wing straight movement, in milliseconds
-const int wingPeriodTurn = 3000; //Period of wing turning movement, in milliseconds
-const int wingCenter = 90; //Center of the servos' motion
-const int wingRange = 75; //Range of motion, degrees, at full power
-
-const int rudderCenter = 80; //Center of the servos' motion
-const int rudderRange = 65; //Rudder range of motion, degrees
-/*const int rudderSpeed = 6; //Degrees per step, maximum rotation speed
-int rudderPos = rudderCenter; //Current position
-int rudderGoal = rudderCenter; //Where it wants to be*/
-
-const int turnTime = 3000; //How long a turn takes, in milliseconds
-const int skullTime = 6000; //Same but for skulling backwards
-
-const int sensorAvgWindow = 10;
-int leftReadings[sensorAvgWindow], rightReadings[sensorAvgWindow];
-int leftAvg = 0, rightAvg = 0;
-
-const int turnThreshhold = 225; //Turning begins when voltage is greater than this
-const int reverseThreshhold = 300; //Skull backwards when voltage > this
-
-
-const int leftSensorPin = A4, rightSensorPin = A5; //Analog pins to read sensors
-const int leftWingPin = 9, rightWingPin = 10, rudderPin = 11; //PWM-capable pins for servos
-const int led = A3; //Output pin to blinking LED
-
-//SharpIR leftSensor(leftSensorPin,20150); //Set up IR sensor objects (20150 is a magic number for this sensor model)
-//SharpIR rightSensor(leftSensorPin,20150);
-
-int left_volt, right_volt; //Analog signals coming from the IR sensors
-
-Servo leftWing, rightWing, rudder; //Servo objects for wings and rudder
-
-int turnDir = 0; //0 for straight, -1 for left, 1 for right, 2 for backwards
-int turnTimer = 0; //Milliseconds left in the turn
-int phase; //Keeps track of the current position in the sine wave for the wings
-long unsigned int timer = 0; //Current position in the wing-motion cycle
-long unsigned int lastTime = 0; //Time the last iteration ended
-long int loopTime = 0; //How long the last loop took -- signed to prevent underflow
-
-void setup() {
-  
-  leftWing.attach(leftWingPin); //Attach servo objects to physical pins
-  rightWing.attach(rightWingPin);
-  rudder.attach(rudderPin);  
-  pinMode(led, OUTPUT); //Set up LED pin
-  Serial.begin(9600);
-
-  for (int i = 0; i < sensorAvgWindow; i++) { //Initialize the averages to max so it doesn't start by turning
-    leftReadings[i] = 0;
-    rightReadings[i] = 0;
-  }
-
-}
-
-void loop() {
-  /*________sense________*/
-  // If turn from last loop isn't finish, this will ensure that it finishes.
-  if (turnTimer>0){
-    turnTimer -= loopTime; //Decrement the turn timer, in milliseconds
-    if (turnTimer < 0) turnTimer = 0;
-  }
-  else {
-    turnDir = 0;
-  }
-
-  // Getting the distances from walls from both sensors (converts from mm to cm)
-  //left_dist = leftSensor.getDistance()*10;
-  //right_dist = rightSensor.getDistance()*10;
-
-  left_volt = analogRead(leftSensorPin);
-  right_volt = analogRead(rightSensorPin);
-
-  Serial.print("Left reading: " + String(left_volt) +"  Right reading: " + String(right_volt));
-  
-  /*________think________*/
-
-  for (int i = 0; i < sensorAvgWindow - 1; i++) {
-    leftReadings[i] = leftReadings[i+1]; //Shift all readings back one space
-    rightReadings[i] = rightReadings[i+1];
-  }
-
-  leftReadings[sensorAvgWindow-1] = left_volt;
-  rightReadings[sensorAvgWindow-1] = right_volt;
-
-  leftAvg = 0; //Reset the averages, so we can re-sum them
-  rightAvg = 0;
-
-  for (int i = 0; i < sensorAvgWindow; i++) { //Sum to average
-    leftAvg += leftReadings[i];
-    rightAvg += rightReadings[i];
-  }
-
-  leftAvg = leftAvg / sensorAvgWindow; //Divide after summing to preserve precision
-  rightAvg = rightAvg / sensorAvgWindow;  
-
-  Serial.print("Left avg: " + String(leftAvg) +"  Right avg: " + String(rightAvg));
-
-
-  // This determines our boat's direction by assigning it a switch case based on sensor output
-  if (leftAvg > turnThreshhold){
-    turnDir = 1;  //Are we too close to a wall?
-    turnTimer = turnTime;  //Sets us to have a 5 sec turn
-  }
-  if (rightAvg > turnThreshhold){
-    turnDir = -1; //If both triggered (but not close enough to skull), turn left
-    turnTimer = turnTime;
-  }
-  if ((rightAvg > reverseThreshhold) && (leftAvg > reverseThreshhold)){
-    turnDir = 2;
-    turnTimer = skullTime;
-  }
- 
-  int rudder_phase = rudderCenter + triangleWaveFunction(timer*2, false) * rudderRange / wingRange;
-
-  /*_________act_________*/
-  switch (turnDir) {
-    //In case 0 the boat will go straight because the sensor did not sense a wall within range.
-    case 0:
-      Serial.println("case 0");
-      leftWing.write(wingCenter - triangleWaveFunction(timer, true));
-      rightWing.write(wingCenter + triangleWaveFunction(timer, true));
-      rudder.write(rudderCenter);
-      Serial.println(rudder.read());
-      break;
-    
-    //In case -1 the boat will turn to the because the left IR sensed a wall within range. 
-    case -1:
-      Serial.println("case -1");
-      leftWing.write(wingCenter - triangleWaveFunction(timer, false));
-      //rightWing.write(wingCenter);
-      rudder.write(rudderCenter + rudderRange/2);
-      Serial.println(rudder.read());
-      Serial.println(rudderCenter - rudderRange);
-      break;
-      
-    //In case 1 the boat will turn to the because the right IR sensed a wall within range.
-    case 1:
-      Serial.println("case 1");
-      //leftWing.write(wingCenter);
-      rightWing.write(wingCenter + triangleWaveFunction(timer, false));
-      rudder.write(rudderCenter - rudderRange/2);
-      //rudderGoal = rudderCenter - rudderRange/2;
-      Serial.println(rudder.read());
-      break;
-    
-    //In case 2 the boat has sensed both walls as being dangerously close and is now using the rudder to reverse.
-    case 2:
-      Serial.println("case 2");
-      //leftWing.write(wingCenter);
-      //rightWing.write(wingCenter);
-      rudder.write(rudder_phase);
-      //rudderGoal = rudder_phase;
-      //rudderPos = rudderGoal; //No soft move for skulling!
-      Serial.println(rudder.read());
-      break;
-  }
-
-  //if (rudderPos > rudderGoal) rudderPos -= rudderSpeed; //Adjust the rudder's position
-  //else if (rudderPos < rudderGoal) rudderGoal += rudderSpeed;
-  //rudder.write(rudderPos); //Actually move the servo
- 
-  timer += millis() - lastTime;
-  timer = timer % wingPeriodStraight; //Prevent overflows
-  loopTime = millis() - lastTime;
-  lastTime = millis(); //So we'll know how long the next loop takes
- 
- //Blink LED while main loop is running
-  if (timer < wingPeriodStraight/2){
-    digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  }
-  else {
-    digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  }
-}
-
-// This is a function to be used for the wing servos. It is a trunkated triangle wave function. 
-// This will make the servos move at a constant rate and uses a fourier transform to imitate a trunkated triangle wave.
-int triangleWaveFunction(int phase, bool isStraight){
-  float x = 0;
-  if (isStraight)
-    x = phase * 2*PI / wingPeriodStraight;
-  else
-    x = phase * 2*PI / wingPeriodTurn;
-    
-  return wingRange/2 * 8*(PI*PI)*(sin(x)-(1/9*sin(3*x))+(1/25*sin(5*x))-(1/49*sin(7*x))) / 100;
-}
->>>>>>> 62a0d0862ecdc92fecc4760cb2c178e93faaedf2
